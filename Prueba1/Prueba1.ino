6e94bce7929225fd1a16a1e71641d1e7cc547b38
Sebastian Carmona <sebascarm@gmail.com>
2018-04-11 10:09:36 -0300

    Agregar archivos de proyecto.

---
+++ Prueba1/Prueba1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Prueba1.ino
+ Created:	11/04/2018 10:09:16
+ Author:	scarmona
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
